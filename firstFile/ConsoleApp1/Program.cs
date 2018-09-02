ebab169694fc3c3d976cb1dadc6f07e631e2cfd7
vorobei <vorobei.yaroslav@gmail.com>
2018-09-02 23:36:45 +0300

    commit to new branch

--- firstFile/ConsoleApp1/Program.cs
+++ firstFile/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("log info level!");
+            Console.WriteLine("test new branch!");
         }
     }
 }
